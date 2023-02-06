5a879bec80f9a4df41ada30bb42bb750d6dcc7ba
OmriBenHur <omribhk2000@gmail.com>
2023-02-06 15:24:10 +0200

    changed acm

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,17 +1,26 @@
-resource "aws_acm_certificate" "cert" {
+resource "aws_route53_zone" "fargate-zone" {
+  name = var.domain-name
+}
+
+resource "aws_acm_certificate" "fargate-certificate" {
   domain_name       = var.domain-name
   validation_method = "DNS"
-
-  tags = {
-    Name = "fargate application certificate"
-  }
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
+resource "aws_route53_record" "fargate-app-dns" {
+  allow_overwrite = true
+  name =  tolist(aws_acm_certificate.fargate-certificate.domain_validation_options)[0].resource_record_name
+  records = [tolist(aws_acm_certificate.fargate-certificate.domain_validation_options)[0].resource_record_value]
+  type = tolist(aws_acm_certificate.fargate-certificate.domain_validation_options)[0].resource_record_type
+  zone_id = aws_route53_zone.fargate-zone.zone_id
+  ttl = 60
+}
+
 resource "aws_acm_certificate_validation" "hello_cert_validate" {
-  certificate_arn = aws_acm_certificate.cert.arn
+  certificate_arn = aws_acm_certificate.fargate-certificate.arn
   validation_record_fqdns = [for o in var.fqdns : o]
 }
